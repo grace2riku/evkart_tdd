c72602c21e61f76a88ec310436c217f9570468b7
Koji Abe <gracehime@gmail.com>
2022-10-31 12:49:04 +0900

    TurnOnHallULedテストを成功させるための実装を書く。TurnOnHallULedテストが成功することを確認した。

--- src/HallSensorLedDriver/HallSensorLedDriver.c
+++ src/HallSensorLedDriver/HallSensorLedDriver.c
@@ -1,12 +1,19 @@
 #include "HallSensorLedDriver.h"
 
+static unsigned char* hallSensorLedsAddress;
+
 void HallSensorLedDriver_Create(unsigned char* address)
 {
-    *address = 0;
+//    *address = 0;
+    hallSensorLedsAddress = address;
+    *hallSensorLedsAddress = 0;
 }
 
 void HallSensorLedDriver_Destroy(void)
 {
 }
 
+void HallSensorLedDriver_TurnOn(unsigned char ledBitNumber) {
+    *hallSensorLedsAddress = 0x04;
+}
 
